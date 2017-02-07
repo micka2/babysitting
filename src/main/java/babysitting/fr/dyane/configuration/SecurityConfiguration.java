a16c744d9b8ceabf498bbf7ffdf3b16bb2ab5aeb
Mickael Duranteau <mickael.duranteau@gmail.com>
2017-02-07 14:59:07 +0100

    add configuration to database

---
+++ src/main/java/babysitting/fr/dyane/configuration/SecurityConfiguration.java
@@ -0,0 +1,23 @@
+package babysitting.fr.dyane.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.csrf().disable();
+
+		httpSecurity.anonymous().and().authorizeRequests().antMatchers("/create").permitAll().antMatchers("/login/**")
+				.permitAll().antMatchers("/createSubject").permitAll().antMatchers("/api/**").permitAll()
+				.antMatchers("/teacher").permitAll().antMatchers("/update").permitAll().antMatchers(HttpMethod.OPTIONS)
+				.permitAll()
+
+		;
+	}
+
+}
